4749666af1718f628e65dacf782046d3f7b5fcc0
Jake Peterson <atw1003p@navitus.com>
2020-05-18 08:51:13 -0500

    Documented attributes. Added index mapping over string indexing.

--- JPCodes.ORM/Attributes.cs
+++ JPCodes.ORM/Attributes.cs
@@ -4,18 +4,27 @@ using System.Text;
 
 namespace JPCodes.ORM
 {
+    /// <summary>
+    /// Set a table name to be different than the class name.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class DataRecordAttribute : Attribute
     {
         public string TableName { get; set; }
     }
 
+    /// <summary>
+    /// Indicates a group of fields are unique in the database.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class UniqueFieldGroupAttribute : Attribute
     {
         public string[] Fields { get; private set; }
     }
 
+    /// <summary>
+    /// Set a DisplayName and FieldName to be different than the property name.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class DataFieldAttribute : Attribute
     {
@@ -23,9 +32,15 @@ namespace JPCodes.ORM
         public string FieldName { get; private set; }
     }
 
+    /// <summary>
+    /// Indicates a field is a primary key in the datasource.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class PrimaryKeyAttribute : DataFieldAttribute { }
 
+    /// <summary>
+    /// Indicates a field is unique in the datasource.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class UniqueFieldAttribute : DataFieldAttribute { }
 }
--- JPCodes.ORM/Extensions.cs
+++ JPCodes.ORM/Extensions.cs
@@ -11,6 +11,8 @@ namespace JPCodes.ORM
 {
     public static class Extensions
     {
+        private static ConcurrentDictionary<string, List<(int IDX, DataField DF)>> _mapCache = new ConcurrentDictionary<string, List<(int IDX, DataField DF)>>();
+
         public static async Task<List<T>> Query<T>(this DbConnection dbConnection, string sql, params (string Name, object Value)[] parameters) where T : new()
         {
             bool opened = dbConnection.State == ConnectionState.Closed;
@@ -25,6 +27,7 @@ namespace JPCodes.ORM
 
                 using (DbCommand command = dbConnection.CreateCommand())
                 {
+                    command.CommandText = sql;
                     foreach ((string name, object value) param in parameters)
                     {
                         DbParameter dbparam = command.CreateParameter();
@@ -34,19 +37,33 @@ namespace JPCodes.ORM
                     }
                     using (DbDataReader rdr = await command.ExecuteReaderAsync())
                     {
-                        List<DataField> resultFields = definition.GetFields(
-                            Enumerable.Range(0, rdr.FieldCount).Select(IDX => rdr.GetName(IDX))
-                        ).ToList();
-
                         try
                         {
+                            if (!_mapCache.TryGetValue(sql, out List<(int index, DataField datafield)> resultFields))
+                            {
+                                List<string> fields = Enumerable.Range(0, rdr.FieldCount).Select(IDX => rdr.GetName(IDX).ToUpper()).ToList();
+
+                                _mapCache[sql] = resultFields = definition.Fields
+                                    .Select((datafield, index) =>
+                                    {
+                                        int idx = fields.IndexOf(datafield.FieldName.ToUpper());
+                                        if (idx >= 0)
+                                        {
+                                            return (idx, datafield);
+                                        }
+                                        return (-1, null);
+                                    })
+                                    .Where(CPL => CPL.idx >= 0)
+                                    .ToList();
+                            }
+
                             List<T> results = new List<T>();
                             while (await rdr.ReadAsync())
                             {
                                 T item = new T();
-                                foreach (DataField field in resultFields)
+                                foreach (var (index, datafield) in resultFields)
                                 {
-                                    field.Set(item, rdr[field.FieldName]);
+                                    datafield.Set(item, rdr[index]);
                                 }
                                 results.Add(item);
                             }
